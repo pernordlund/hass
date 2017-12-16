b7a11601d2e863cd2524c5e692480f5dc95d762e
Per Nordlund <per.nordlund@panitco.se>
2017-12-16 13:49:17 +0100

    wip

--- python_scripts/get_bus_time.py
+++ python_scripts/get_bus_time.py
@@ -4,5 +4,10 @@ Script to retrieve bus time from SL-API
 import json
 import requests
 
+resource = redacted
+resp = requests.get(resource)
 
-
+if resp.status_code == 200:
+    for bus in json.loads(resp.text)['ResponseData']['Buses']:
+	    if bus['Destination'] == 'Slussen':
+		    print(bus['DisplayTime'])
