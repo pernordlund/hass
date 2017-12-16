76d91320779b8125391700596c44425e9f921a63
Per Nordlund <per.nordlund@panitco.se>
2017-12-16 15:56:38 +0000

    Autocommit

--- python_scripts/get_bus_time.py
+++ python_scripts/get_bus_time.py
@@ -1,13 +1,21 @@
 """
 Script to retrieve bus time from SL-API
 """
-import json
-import requests
+#import json
+#import requests
+message = ''
 
-resource = redacted
-resp = requests.get(resource)
+resource = data.get('url','not given')
+logger.warning("url {}".format(resource))
 
-if resp.status_code == 200:
-    for bus in json.loads(resp.text)['ResponseData']['Buses']:
-	    if bus['Destination'] == 'Slussen':
-		    print(bus['DisplayTime'])
+#resp = requests.get(resource)
+#if (resp.status_code == 200) and json.loads(resp.text)['ResponseData']:
+#    for bus in json.loads(resp.text)['ResponseData']['Buses']:
+#        if bus['Destination'] == 'Slussen':
+#            message = message + bus['DisplayTime'] + chr(10)
+#    if not message:
+#	    message = 'No buses for Slussen within 30 minutes'
+#else:
+#    message = 'Error calling SL-api'
+
+hass.services.call('notify', 'pelles_tfn', { "message" : resource })
