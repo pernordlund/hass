71bf4f7161bfde69785de23e37581f6e780e62c4
Per Nordlund <per.nordlund@panitco.se>
2018-01-15 08:32:59 +0100

    testing

--- python_scripts/get_bus_time.py
+++ python_scripts/get_bus_time.py
@@ -1,21 +1,23 @@
 """
 Script to retrieve bus time from SL-API
 """
-#import json
-#import requests
-message = ''
+import json
+import requests
+message = 'default'
 
-resource = data.get('url','not given')
-logger.warning("url {}".format(resource))
+resource = 'http://api.sl.se/api2/realtimedeparturesv4.json?key=14df1b5b39e744c0ba9d78f0abb89bdc&siteid=4612&timewindow=15'
+#resource = data.get('url','not given')
+#logger.warning("url {}".format(resource))
 
-#resp = requests.get(resource)
-#if (resp.status_code == 200) and json.loads(resp.text)['ResponseData']:
-#    for bus in json.loads(resp.text)['ResponseData']['Buses']:
-#        if bus['Destination'] == 'Slussen':
-#            message = message + bus['DisplayTime'] + chr(10)
-#    if not message:
-#	    message = 'No buses for Slussen within 30 minutes'
-#else:
-#    message = 'Error calling SL-api'
+resp = requests.get(resource)
+if (resp.status_code == 200) and json.loads(resp.text)['ResponseData']:
+    for bus in json.loads(resp.text)['ResponseData']['Buses']:
+        if bus['Destination'] == 'Slussen':
+            message = message + bus['DisplayTime'] + chr(10)
+    if not message:
+	    message = 'No buses for Slussen within 30 minutes'
+else:
+   message = 'Error calling SL-api'
 
-hass.services.call('notify', 'pelles_tfn', { "message" : resource })
+print message
+#hass.services.call('notify', 'pelles_tfn', { "message" : resource })
