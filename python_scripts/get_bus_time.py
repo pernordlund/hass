8f7510a18fe3fc7c2b027ccc54d5e4d85de9dc64
Per Nordlund <Per.Nordlund@okq8.se>
2018-01-15 22:34:14 +0100

    cleanup

--- python_scripts/get_bus_time.py
+++ python_scripts/get_bus_time.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 resourceSL = 'http://api.sl.se/api2/realtimedeparturesv4.json?key=14df1b5b39e744c0ba9d78f0abb89bdc&siteid=4612&timewindow=30'
 resourceHA = 'http://192.168.1.33:8123/api/states/sensor.slussen'
+fn = {"friendly_name": "Slussenbuss"} 
 
 resp = requests.get(resourceSL)
 jsonData = json.loads(resp.text)
@@ -19,9 +20,6 @@ if (resp.status_code == 200) and ('ResponseData' in jsonData):
 else:
    message = 'Error calling SL-api'
 
-#payload = '{"state": "ssKlisatra", "attributes": {"Bus 1": \"' + message + '\",' + \
-#    '"Bus2": \"' + str(datetime.now()) + '\"}}'
-fn = {"friendly_name": "Slussenbuss"} 
 payload = json.dumps({"state": message, "attributes": {**jsonData,**fn}})
 
 resp = requests.post(resourceHA, data=payload)
